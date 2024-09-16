1bff62fd9a429234a767c8bb882ad85e5e036e70
Hoffmano <hoffman.devs@gmail.com>
2024-09-16 12:29:48 -0300

    aa

--- config.sh
+++ config.sh
@@ -1,11 +1,13 @@
+# ??? não sei qual ppa é esse ai
+sudo add-apt-repository ppa:aslatter/ppa
+
+# neovim ppa
+sudo add-apt-repository ppa:neovim-ppa/unstable
+
 sudo apt update
 
-sudo apt-get install gh snapd ripgrep fd-find gnome-tweaks -y
+sudo apt-get install alacritty gh snapd ripgrep fd-find gnome-tweaks vivaldi-stable -y
 
-sudo snap install alacritty --classic
-sudo snap install nvim --classic
-
-sudo snap install vivaldi
 # Customizar o Vivaldi
 # https://github.com/tovifun/VivalArc
 
@@ -28,13 +30,19 @@ font_url='https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBr
 font_name=${font_url##*/}
 wget -nc ${font_url} && unzip -o ${font_name} -d ~/.fonts && fc-cache -fv && echo 'nerd font'
 
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-lazygit --version
+if command -v lazygit >/dev/null; then
+  echo 'lazygit already installed'
+else
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  sudo install lazygit /usr/local/bin
+  lazygit --version
+  rm lazygit.tar.gz
+fi
 
-# install gnome theme
+curl -sS https://starship.rs/install.sh | sh
+
 echo 'Install Gnome Themes'
 echo 'Enter: https://www.gnome-look.org/browse'
 echo 'Extract files to .icons and .themes'
@@ -42,7 +50,7 @@ cd
 mkdir -p .themes
 mkdir -p .icons
 while true; do
-  read -p "Do you want to continue?" yn
+  read -p "Do you want to continue? yn" yn
   case $yn in
   [Yy]*) break ;;
   [Nn]*) exit ;;
@@ -50,9 +58,9 @@ while true; do
   esac
 done
 
-gh auth login
 git config --global user.email "hoffman.devs@gmail.com"
 git config --global user.name "Hoffmano"
+gh auth login
 
 sudo apt upgrade -y
 sudo apt autoremove -y && sudo apt clean -y
